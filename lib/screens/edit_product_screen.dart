281d2a2f4f5566bb47ba3fdb58f745d2b10662a2
Salma Gambo <gambosalma31@gmail.com>
2023-04-30 17:51:11 +0100

    Salma's commit

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -9,11 +9,13 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
 
   @override
   void dispose(){
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
     super.dispose();
   }
 
@@ -56,6 +58,30 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 FocusScope.of(context).requestFocus(_descriptionFocusNode);
               },
             ),
+            Row(
+              children: [
+                Container(
+                 width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(top: 8,right: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.grey
+                    )
+                  ),
+                  child:_imageUrlController.text.isEmpty ? Text('Enter a URl') : FittedBox(child: Image.network(_imageUrlController.text) ,),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Imagr URL',
+                  ),
+                  keyboardType: TextInputType.url,
+                  textInputAction: TextInputAction.done,
+                  controller:_imageUrlController,
+                )
+              ],
+            )
           ],
         )),
       ),
